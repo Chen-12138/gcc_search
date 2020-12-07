4f429d9efe8df75d15e0c93ba2f9c61cb31bcf36
Chen-12138 <2464774230@qq.com>
2020-12-07 10:39:08 +0800

    删除mint-ui

--- src/main.js
+++ src/main.js
@@ -2,12 +2,9 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import MintUI from 'mint-ui'
-import 'mint-ui/lib/style.css'
 import '@/assets/css/common.css'
 
 Vue.config.productionTip = false
-Vue.use(MintUI)
 
 new Vue({
   router,
