0488f4bf2b9a638ccc2bfbec8189ad8b188d3209
warlock <1163675107@qq.com>
2020-12-08 11:54:46 +0800

    [add]header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,23 +1,101 @@
 <template>
-    <header id="header">
-        <slot></slot> <h1>{{title}}</h1>
-    </header>
+  <header id="header">
+    <div class="wrapper">
+      <div class="menu"  @click="openMenu">
+        <div class="menu-icon">
+          <i class="fa fa-align-justify" aria-hidden="true"></i>
+        </div>
+        <div v-if="open" class="menu-main">
+         <ul>
+             <li>1</li>
+             <li>2</li>
+             <li>3</li>
+             <li>4</li>
+         </ul>
+        </div>
+      </div>
+      <div class="image-container">
+        <img src="@/assets/images/ci.png" id="imageCi" />
+      </div>
+      <div class="main-container">
+        <i class="fa fa-search" aria-hidden="true"></i>
+        <i class="fa fa-user" aria-hidden="true"></i>
+      </div>
+    </div>
+    <p>Catonese color culture</p>
+  </header>
 </template>
 
 <script>
 export default {
-    name : 'Header',
-    props : {
-        title : {
-            type : String,
-            default : '粤彩文化'
-        }
+  name: "",
+  data(){
+   return{
+     open:false
+   }
+  },
+  props: {
+    title: {
+      type: String,
+      default: "粤彩文化",
+    },
+  },
+  methods:{
+    openMenu(){
+        this.open=!this.open;
     }
-}
+  }
+};
 </script>
 
-<style scoped>
-#header{width:100%; height:50px; color: #fff; background: #e54847; border-bottom: 1px solid #e54847; position: relative;}
-#header h1{ font-size: 18px; text-align: center; line-height: 50px; font-weight: normal; }
-#header i{ position: absolute; left: 5px; top: 50%; margin-top: -13px; font-size: 26px;}
+<style scoped lang="scss">
+#header {
+  width: 100%;
+  height: 80px;
+  padding: 0 1.8rem;
+  color: #fff;
+  background: #e54847;
+  font-size: 1.7rem;
+  position: relative;
+  .wrapper {
+    width: 100%;
+    height:calc(100% - 1rem);
+    display: flex;
+    align-items: flex-end;
+    .menu {
+      width: 60px;
+      text-align: left;
+      .menu-main{
+          position: absolute;
+          top:100%;
+          left:0;
+          background: #f09292;
+          width:80%;
+          li{
+              border-bottom: 1px rgba(255, 255, 255, 0.3) solid;
+          }
+        
+      }
+    }
+    .image-container {
+      display: flex;
+      flex: 1;
+      flex-direction: column;
+      align-items: center;
+      img {
+        width: 68px;
+        height: 64px;
+      }
+    }
+    .main-container {
+      display: flex;
+      justify-content: space-between;
+      width: 60px;
+    }
+  }
+  p{  
+      font-size: 1rem;
+      transform: scale(0.5) translateY(-1rem);
+  }
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -2,7 +2,9 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+
 import '@/assets/css/common.css'
+import 'font-awesome/css/font-awesome.css'
 
 Vue.config.productionTip = false
 
