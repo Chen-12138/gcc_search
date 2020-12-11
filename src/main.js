7bd91e17f6a548d8f5e703961713f43a2afe9e47
bravedreamer <1074429476@qq.com>
2020-12-11 22:46:26 +0800

    [add] 引入vant ui,并做自适应处理

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
+import 'amfe-flexible';
 import '@/assets/css/common.css'
 import 'font-awesome/css/font-awesome.css'
 
